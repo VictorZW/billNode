f88e512f71e7fbd040afdc0f8ac70e388e6e86ba
zhangwei <zhangwei01@zto.com>
2020-01-10 13:57:12 +0800

    数据按时间倒序

--- db/billSql.js
+++ db/billSql.js
@@ -5,7 +5,7 @@
  **/
 const billSQL = {
   insert: 'INSERT INTO bill(pay_date, cost, category, remark, token) VALUES(?,?,?,?,?)',
-  queryAll: 'SELECT * FROM bill WHERE token = ?',
+  queryAll: 'SELECT * FROM bill WHERE token = ? ORDER BY pay_date DESC',
   getUserById: 'SELECT * FROM bill WHERE id = ? ',
   delUser: 'DELETE FROM bill WHERE id = ?',
   updateUserInfo: 'UPDATE bill SET pay_date=?, cost=?, category=?, remark=? WHERE userId = ?'
